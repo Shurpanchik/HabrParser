5d5d1afba8162fd3adba2aea017fef6eb7d0f3d7
Shurpanchik <Shur1993@mail.ru>
2017-10-05 12:23:06 +0300

    перебираем все страницы хабра

--- parser.py
+++ parser.py
@@ -6,9 +6,22 @@ soup = BeautifulSoup(urllib.request.urlopen("https://habrahabr.ru/").read())
 
 
 def print_actual_test_article():
+    numberSite = int(1)
     text = ""
-    for link in  soup.find_all('a'):
-        if  str(link.contents[0]).__contains__("Разработка"):
-            text= text + "<br>"+ link.contents[0]+" "+link.get("href")
-    #print(text)
+    while (True):
+        try:
+            ''' Поиск ссылок содержащее слово разарботка и вывод результатов в консоль '''
+            url = "https://habrahabr.ru/page" + str(numberSite)
+            print(url)
+            soup = BeautifulSoup(urllib.request.urlopen(url).read(),'html.parser')
+
+            for link in soup.find_all("article"):
+                title = link.findNext("h2", {"class": "post__title"}).findNext("a", {"class": "post__title_link"})
+                if str(title.contents[0]).__contains__("Разработка"):
+                    text = text + "<br>" +  title.contents[0] + " " + title.get("href")
+
+            numberSite = numberSite + 1
+        except:
+            break
+
     return text
